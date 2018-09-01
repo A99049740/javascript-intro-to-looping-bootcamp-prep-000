5856233dc92a8bc7a613bfe64b21f96c478b7ddc
jill zhang <talktojillzhang@gmail.com>
2018-09-01 01:40:08 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,7 +1,7 @@
 function forLoop(array){
-  for (var i = 1; i<26;i++){
+  for (let i = 1; i<26;i++){
     if (i ==1){
-      array.push("I am ${i} strange loop.");
+      array.push(`I am ${i} strange loop.`);
     }
     else{
       array.push(`I am ${i} strange loops.`)
