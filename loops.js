4bc1e415348235110f6c05a0ddd971ea8c51548b
jill zhang <talktojillzhang@gmail.com>
2018-09-01 00:00:24 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,32 @@
+function forLoop(array){
+  for (var i = 0; i<25;i++){
+    if (i ==1){
+      array.push(`I am ${i} strange loop.`);
+    }
+    else{
+      array.push(`I am ${i} strange loops.`)
+    }
+  }
+  return;
+}
+function whileLoop(number){
+  let countdown = number
+  while (countdown >0){
+    console.log(countdown)
+    countdown--
+  }
+  return "done"
+}
+
+var i = 0;
+function incrementVariable() {
+  i = i + 1;
+}
+
+function doWhileLoop(array){
+  do{
+    array.shift()
+  }
+  while(array.length>0 && incrementVariable())
+  return array
+}
